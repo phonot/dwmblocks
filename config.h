6f69de7d138c9c42698163f3b4ae0f2df70e8e9d
PH <127600177+phonot@users.noreply.github.com>
2023-11-03 20:48:51 +0530

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	//{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	//{"",	"sb-tasks",	10,	26},
 	/* {"",	"sb-music",	0,	11}, */
 	//{"",	"sb-pacpackages",	0,	8},
